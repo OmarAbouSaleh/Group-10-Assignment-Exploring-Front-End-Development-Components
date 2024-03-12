7fa3631c76feca460faa990f95f372f4b2ef3047
louiebadillo <marklouie.badillo@edu.sait.ca>
2024-03-12 00:30:40 -0600

    Arranged Form and List. Removed the form and footer incorporated in list.

--- app/page.js
+++ app/page.js
@@ -3,13 +3,32 @@ import React from "react";
 import Footer from "@/app/components/Footer.jsx";
 import NavBar from "../app/components/NavBar";
 import StudentList from "@/app/components/StudentList";
+import AddStudentForm from "./components/AddStudentForm";
 
 
-export default function Home() {
+
+// export default function Home() {
+//   return (
+//     <main >
+//       <NavBar/>
+//       <StudentList/>
+//     </main>
+//   );
+// }
+
+export default function Home(){
   return (
-    <main >
-      <NavBar/>
-      <StudentList/>
-    </main>
+    <div className="flex flex-col min-h-screen">
+      <NavBar />
+      <div className="flex-grow flex flex-row p-4 space-x-4">
+        <div className="w-1/2">
+          <AddStudentForm/>
+        </div>
+        <div className="w-1/2 overflow-auto">  
+          <StudentList/>
+        </div>
+      </div>
+      <Footer/>
+    </div>
   );
 }
